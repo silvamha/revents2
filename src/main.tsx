78f15e7a69e28b593bb8e314016f01436ccdc035
Ed <silva.email@yahoo.com>
2024-03-10 05:45:16 -0500

    Add empty lines in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './app/layout/App.tsx'
 import "./app/layout/styles.css"
 
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
